73d02e8e3c18710144da9d964de55ff6169b1e50
Christopher Tompkins <chtompkins026@gmail.com>
2019-02-22 20:45:53 +0000

    Automatically backed up by Learn

--- src/createStore.js
+++ src/createStore.js
@@ -1,25 +1,22 @@
-let store; 
-
 export default function createStore(reducer) {
   let state;
- 
+
   function dispatch(action) {
-    state = reducer(state, action);
-    if (store) render();
+    state = reducer(state, action)
+    render()
   }
- 
+
   function getState() {
     return state;
   }
-  
-  dispatch({ type: '@@INIT' });
-  
+
+  dispatch({ type: '@@INIT' })
+
   return {
     dispatch,
     getState
-  };
+  }
 }
-
 
 function render() {
   const container = document.getElementById('container');
